26f23a7d837be54a22c28a08b8a6fc93cc3b564e
rahmatalishah <93567594+rahmatalishah@users.noreply.github.com>
2021-11-02 09:32:56 +0500

    Add files via upload

---
+++ Football.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package templatemethod;
+
+/**
+ *
+ * @author fa18-bse-091
+ */
+public class Football extends Game {
+
+   @Override
+   void endPlay() {
+      System.out.println("Football Game Finished!");
+   }
+
+   @Override
+   void initialize() {
+      System.out.println("Football Game Initialized! Start playing.");
+   }
+
+   @Override
+   void startPlay() {
+      System.out.println("Football Game Started. Enjoy the game!");
+   }
+}
---
+++ TemplatePatternDemo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package templatemethod;
+
+/**
+ *
+ * @author fa18-bse-091
+ */
+public class TemplatePatternDemo {
+    public static void main(String[] args) {
+
+      Game game = new Cricket();
+      game.play();
+      System.out.println();
+      game = new Football();
+      game.play();		
+   }
+    
+}
